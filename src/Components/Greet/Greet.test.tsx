b1b9afd85d3824f34f189f18e56cdb12f0f7aa3d
priyanshuhaldar007 <priyanshu1843@gmail.com>
2024-02-19 20:51:33 +0530

    feat: add multiple grouping test

--- src/Components/Greet/Greet.test.tsx
+++ src/Components/Greet/Greet.test.tsx
@@ -6,19 +6,20 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./Greet"
 
-describe('Greet', ()=>  {
+describe('Greet', () => {
 
-    test('renders correctly',()=> {
-        render(<Greet/>)
-const textElement = screen.getByText('Hello');
-expect(textElement).toBeInTheDocument();
+    test('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello');
+        expect(textElement).toBeInTheDocument();
+    })
 })
-
-test('renders with name', () => {
-    const name = 'vishwas'
-    render(<Greet name={name} />);
-    const textElement = screen.getByText(`Hello ${name}`);
-    expect(textElement).toBeInTheDocument();
-})
+describe('Greet 2', () => {
+    test('renders with name', () => {
+        const name = 'vishwas'
+        render(<Greet name={name} />);
+        const textElement = screen.getByText(`Hello ${name}`);
+        expect(textElement).toBeInTheDocument();
+    })
 
 })

f532862e8b3b1680c9f83d5f839630dedd4bacf5
priyanshuhaldar007 <priyanshu1843@gmail.com>
2024-02-19 19:58:43 +0530

    feat: added basic boilerplate and created two tests

---
+++ src/Components/Greet/Greet.test.tsx
@@ -0,0 +1,20 @@
+/*
+ * Greet should render the text hello and if any name is passed into the component
+ * It should render hello followed by the name
+ */
+
+import { render, screen } from "@testing-library/react"
+import { Greet } from "./Greet"
+
+test('greet renders correctly',()=>{
+    render(<Greet/>)
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+})
+
+test('greet renders with name',()=>{
+    const name = 'vishwas'
+    render(<Greet name={name}/>);
+    const textElement = screen.getByText(`Hello ${name}`);
+    expect(textElement).toBeInTheDocument();
+})
